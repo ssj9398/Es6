f46b93215053ec4ad4d14a2d44ba3df00be1add6
alisyabob <sjs939883@gmail.com>
2021-09-26 01:07:45 +0900

    회원 등록 구현

--- hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/a")
+    @GetMapping("/")
     public String home(){
         return "home";
     }
