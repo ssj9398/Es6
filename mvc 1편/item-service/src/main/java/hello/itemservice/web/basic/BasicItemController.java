86b96a5bf95d4cc594766149763d61d58ee4a66f
alisyabob <sjs939883@gmail.com>
2021-12-30 16:03:17 +0900

    PRG Post/Redirect/Get

--- mvc 1편/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ mvc 1편/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -72,13 +72,22 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV4(Item item){
         itemRepository.save(item);
 
         //   model.addAttribute("item",item);
 
         return "basic/item";
+    }
+
+    /**
+     * PRG - Post/Redirect/Get
+     */
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
     }
 
     @GetMapping("/{itemId}/edit")
