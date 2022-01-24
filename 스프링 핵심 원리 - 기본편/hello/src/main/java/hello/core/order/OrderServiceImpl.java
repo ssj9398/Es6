0d1c2ac71227f0b1f60c5bd905e6290bbac8a47a
alisyabob <sjs939883@gmail.com>
2022-01-24 23:21:02 +0900

    [ADD] 다양한 의존관계 주입 방법

--- 스프링 핵심 원리 - 기본편/hello/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링 핵심 원리 - 기본편/hello/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,17 +12,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private  MemberRepository memberRepository;
+    private  DiscountPolicy discountPolicy;
     //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     //private DiscountPolicy discountPolicy;
 
+    @Autowired(required = false)
+    public void setMemberRepository(MemberRepository memberRepository){
+        System.out.println("memberRepository = " + memberRepository);
+        this.memberRepository = memberRepository;
+    }
 
     @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
+    public void setDiscountPolicy(DiscountPolicy discountPolicy){
+        System.out.println("memberRepository = " + discountPolicy);
         this.discountPolicy = discountPolicy;
     }
+
+//    @Autowired
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
