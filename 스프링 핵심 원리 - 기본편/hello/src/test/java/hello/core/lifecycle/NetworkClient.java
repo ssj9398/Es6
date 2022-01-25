b02d62d89d1e46abc6705ce904840a0b98126ca3
alisyabob <sjs939883@gmail.com>
2022-01-25 15:23:25 +0900

    [ADD] 인터페이스 InitializingBean, DisposableBean

--- 스프링 핵심 원리 - 기본편/hello/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ 스프링 핵심 원리 - 기본편/hello/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,15 +1,14 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient {
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url) {
@@ -28,4 +27,17 @@ public class NetworkClient {
         System.out.println("close = " + url);
     }
 
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
 }
